be11bccffc361516daed71631b49e59f684000a7
Earlopain <14981592+Earlopain@users.noreply.github.com>
2026-06-12 19:05:15 +0200

    Fewer specs for Kernel
    Currently it checks that `Kernel.method` == `Kernel.instance_method` by running specs twice
    
    But the Kernel methods are just mixed into Object, so that is a bit redundant.

--- core/kernel/chomp_spec.rb
+++ core/kernel/chomp_spec.rb
@@ -2,45 +2,33 @@
 require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 
-describe :kernel_chomp, shared: true do
+describe "Kernel#chomp" do
+  it "is a private method" do
+    KernelSpecs.private_instance_method?(:chomp).should == true
+  end
+
   it "removes the final newline of $_" do
-    KernelSpecs.chomp("abc\n", @method).should == "abc"
+    KernelSpecs.chomp("abc\n").should == "abc"
   end
 
   it "removes the final carriage return of $_" do
-    KernelSpecs.chomp("abc\r", @method).should == "abc"
+    KernelSpecs.chomp("abc\r").should == "abc"
   end
 
   it "removes the final carriage return, newline of $_" do
-    KernelSpecs.chomp("abc\r\n", @method).should == "abc"
+    KernelSpecs.chomp("abc\r\n").should == "abc"
   end
 
   it "removes only the final newline of $_" do
-    KernelSpecs.chomp("abc\n\n", @method).should == "abc\n"
+    KernelSpecs.chomp("abc\n\n").should == "abc\n"
   end
 
   it "removes the value of $/ from the end of $_" do
-    KernelSpecs.chomp("abcde", @method, "cde").should == "ab"
+    KernelSpecs.chomp("abcde", "cde").should == "ab"
   end
-end
-
-describe :kernel_chomp_private, shared: true do
-  it "is a private method" do
-    KernelSpecs.has_private_method(@method).should == true
-  end
-end
-
-describe "Kernel.chomp" do
-  it_behaves_like :kernel_chomp, "Kernel.chomp"
 end
 
 describe "Kernel#chomp" do
-  it_behaves_like :kernel_chomp, "chomp"
-
-  it_behaves_like :kernel_chomp_private, :chomp
-end
-
-describe :kernel_chomp_encoded, shared: true do
   before :each do
     @external = Encoding.default_external
     Encoding.default_external = Encoding::UTF_8
@@ -51,15 +39,13 @@ describe :kernel_chomp_encoded, shared: true do
   end
 
   it "removes the final carriage return, newline from a multi-byte $_" do
-    script = fixture __FILE__, "#{@method}.rb"
+    script = fixture __FILE__, "chomp.rb"
     KernelSpecs.run_with_dash_n(script).should == "あれ"
   end
 end
 
 describe "Kernel.chomp" do
-  it_behaves_like :kernel_chomp_encoded, "chomp"
-end
-
-describe "Kernel#chomp" do
-  it_behaves_like :kernel_chomp_encoded, "chomp_f"
+  it "is a public method" do
+    KernelSpecs.public_singleton_method?(:chomp).should == true
+  end
 end
--- core/kernel/sprintf_spec.rb
+++ core/kernel/sprintf_spec.rb
@@ -1,64 +1,97 @@
 require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 require_relative 'shared/sprintf'
-require_relative 'shared/sprintf_encoding'
 
-describe :kernel_sprintf_to_str, shared: true do
+describe "Kernel#sprintf" do
+  it "is a private method" do
+    Kernel.private_instance_methods(false).should.include?(:sprintf)
+  end
+
+  it_behaves_like :kernel_sprintf, -> format, *args {
+    r = nil
+    -> {
+      r = sprintf(format, *args)
+    }.should_not complain(verbose: true)
+    r
+  }
+
   it "calls #to_str to convert the format object to a String" do
     obj = mock('format string')
     obj.should_receive(:to_str).and_return("to_str: %i")
-    @method.call(obj, 42).should == "to_str: 42"
+    sprintf(obj, 42).should == "to_str: 42"
+  end
+
+  # Keep encoding-related specs in a separate shared example to be able to skip them in IO/File/StringIO specs.
+  # It's difficult to check result's encoding in the test after writing to a file/io buffer.
+  context "encoding" do
+    it "can produce a string with valid encoding" do
+      string = sprintf("good day %{valid}", valid: "e")
+      string.encoding.should == Encoding::UTF_8
+      string.valid_encoding?.should == true
+    end
+
+    it "can produce a string with invalid encoding" do
+      string = sprintf("good day %{invalid}", invalid: "\x80")
+      string.encoding.should == Encoding::UTF_8
+      string.valid_encoding?.should == false
+    end
+
+    it "returns a String in the same encoding as the format String if compatible" do
+      string = "%s".dup.force_encoding(Encoding::KOI8_U)
+      result = sprintf(string, "dogs")
+      result.encoding.should.equal?(Encoding::KOI8_U)
+    end
+
+    it "returns a String in the argument's encoding if format encoding is more restrictive" do
+      string = "foo %s".dup.force_encoding(Encoding::US_ASCII)
+      argument = "b\303\274r".dup.force_encoding(Encoding::UTF_8)
+
+      result = sprintf(string, argument)
+      result.encoding.should.equal?(Encoding::UTF_8)
+    end
+
+    it "raises Encoding::CompatibilityError if both encodings are ASCII compatible and there are not ASCII characters" do
+      string = "Ä %s".encode('windows-1252')
+      argument = "Ђ".encode('windows-1251')
+
+      -> {
+        sprintf(string, argument)
+      }.should.raise(Encoding::CompatibilityError)
+    end
+
+    describe "%c" do
+      it "supports Unicode characters" do
+        result = sprintf("%c", 1286)
+        result.should == "Ԇ"
+        result.bytes.should == [212, 134]
+
+        result = sprintf("%c", "ش")
+        result.should == "ش"
+        result.bytes.should == [216, 180]
+      end
+
+      it "raises error when a codepoint isn't representable in an encoding of a format string" do
+        format = "%c".encode("ASCII")
+
+        -> {
+          sprintf(format, 1286)
+        }.should.raise(RangeError, /out of char range/)
+      end
+
+      it "uses the encoding of the format string to interpret codepoints" do
+        format = "%c".dup.force_encoding("euc-jp")
+        result = sprintf(format, 9415601)
+
+        result.encoding.should == Encoding::EUC_JP
+        result.should == "é".encode(Encoding::EUC_JP)
+        result.bytes.should == [143, 171, 177]
+      end
+    end
   end
 end
 
-describe "Kernel#sprintf" do
-  it_behaves_like :kernel_sprintf, -> format, *args {
-    r = nil
-    -> {
-      r = sprintf(format, *args)
-    }.should_not complain(verbose: true)
-    r
-  }
-
-  it_behaves_like :kernel_sprintf_encoding, -> format, *args {
-    r = nil
-    -> {
-      r = sprintf(format, *args)
-    }.should_not complain(verbose: true)
-    r
-  }
-
-  it_behaves_like :kernel_sprintf_to_str, -> format, *args {
-    r = nil
-    -> {
-      r = sprintf(format, *args)
-    }.should_not complain(verbose: true)
-    r
-  }
-end
-
 describe "Kernel.sprintf" do
-  it_behaves_like :kernel_sprintf, -> format, *args {
-    r = nil
-    -> {
-      r = Kernel.sprintf(format, *args)
-    }.should_not complain(verbose: true)
-    r
-  }
-
-  it_behaves_like :kernel_sprintf_encoding, -> format, *args {
-    r = nil
-    -> {
-      r = Kernel.sprintf(format, *args)
-    }.should_not complain(verbose: true)
-    r
-  }
-
-  it_behaves_like :kernel_sprintf_to_str, -> format, *args {
-    r = nil
-    -> {
-      r = Kernel.sprintf(format, *args)
-    }.should_not complain(verbose: true)
-    r
-  }
+  it "is a public method" do
+    Kernel.public_methods(false).should.include?(:sprintf)
+  end
 end
